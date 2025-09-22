64f00579fe35360a7222450fb40461309d299146
Lucas <lucasreissilvino@gmail.com>
2025-09-22 18:25:55 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,10 @@
+public class D{
+    private int D1, D2;
+
+    public void MD1(){
+        System.out.println("método MD1");
+    }
+    public void MD2(){
+        System.out.println("método MD1");
+    }
+}
