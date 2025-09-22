30551b0190d98b857f9c579145d9b0b0b4c0ebf6
Lucas <lucasreissilvino@gmail.com>
2025-09-22 15:37:32 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,12 @@
+public class C{
+    private String C1;
+    private int C2;
+
+    public void MC1(){
+        System.out.println("método MC1");
+    }
+
+    public void MC2(){
+        System.out.println("método MC2");
+    }
+}
